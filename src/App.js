0bcf6121f72b9ef7d2b10f943c8ed33cb692beaf
Mayank Tanwar <mayanktanwar8402@gmail.com>
2021-12-12 15:33:27 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import { useEffect } from "react";
 import Aos from "aos";
-import "aos/dist/aos.css"
+import "aos/dist/aos.css";
 function App() {
   useEffect(() => {
     Aos.init({
@@ -16,14 +16,18 @@ function App() {
 
   return (
     <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
-      <div data-aos="fade-down" data-aos-duration="800" >
+      <div data-aos="fade-down" data-aos-duration="800">
         <Card />
       </div>
-      <div data-aos="fade-up" data-aos-duration="800" data-aos-delay='400'>
+      <div data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
         <About />
       </div>
-      <Skills />
-      <Contact />
+      <div data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
+        <Skills />
+      </div>
+      <div data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
+        <Contact />
+      </div>
       <Footer />
     </div>
   );
